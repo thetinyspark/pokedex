b536f60b18c398a1210d64556f5229a1f8861c4a
thetinyspark <contact@the-tiny-spark.com>
2021-04-07 10:55:39 +0200

    add pipe unit test

--- src/app/by-type.pipe.spec.ts
+++ src/app/by-type.pipe.spec.ts
@@ -1,8 +1,45 @@
 import { ByTypePipe } from './by-type.pipe';
+import { Pokemon, POKEMON_LIST, POKEMON_TYPES } from './pokemon';
 
-describe('ByTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ByTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
+describe(
+  'ByTypePipe test suite',
+  () => {
+
+    fit(
+      'create an instance',
+      () => {
+        const pipe = new ByTypePipe();
+        expect(pipe).toBeTruthy();
+      }
+    );
+
+    fit(
+      'should filter pokemons by their type',
+      () => {
+        const pipe = new ByTypePipe();
+        const currentType:string =  POKEMON_LIST[0].type;
+        const results:Pokemon[] = pipe.transform(POKEMON_LIST, currentType);
+
+        results.forEach( 
+          (pokemon:Pokemon) => {
+            expect(pokemon.type).toEqual(currentType);
+          }
+        ); 
+
+      }
+    );
+
+    fit(
+      'should returns no pokemon',
+      () => {
+        const pipe = new ByTypePipe();
+        const currentType:string =  "no_existent_type";
+        const types:string[] = POKEMON_TYPES;
+        const results:Pokemon[] = pipe.transform(POKEMON_LIST, currentType);
+
+        expect(types).not.toContain(currentType);
+        expect(results.length).toEqual(0);
+      }
+    );
+  }
+);
